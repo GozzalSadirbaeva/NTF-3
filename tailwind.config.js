79d7e98f1b96cbef1c9be82a6eaa17e03bb962c2
GozzalSadirbaeva <sadirbaevagozzal@gmail.com>
2024-09-07 23:45:15 +0500

    Done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'violet': '#A259FF',
+      'gray': '#CCCCCC',
+      'gray2': '#3B3B3B',
+      'gray3': '#858584',
+      'white': '#FFFFFF',
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
